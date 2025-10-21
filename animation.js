8f2bcf0aa1a951665d46b8b0280c064b11a914cf
Connor Lau <Connorlau@hotmail.com>
2025-10-20 21:28:45 -0400

    Update with latest changes

--- animation.js
+++ animation.js
@@ -69,3 +69,10 @@ window.addEventListener('load', () => {
   revealImages();
   revealText();
 });
+
+const toggle = document.getElementById('menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
