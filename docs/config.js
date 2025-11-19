7c5dddd3acbf7bf69493a9b33038caa78b48e914
Connor Lau <Connorlau@hotmail.com>
2025-11-19 09:42:20 -0500

    add missing config file

--- docs/config.js
+++ docs/config.js
@@ -1,4 +1,4 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAd-LuABfQtKfugGXYY20_S1uwwyNV6ZVw",
   authDomain: "star-chef-restaurant.firebaseapp.com",
   projectId: "star-chef-restaurant",
@@ -7,3 +7,4 @@ export const firebaseConfig = {
   appId: "1:924721320321:web:41644950fe66ee58eed880",
   measurementId: "G-BKD21L9889"
 };
+export { firebaseConfig };
